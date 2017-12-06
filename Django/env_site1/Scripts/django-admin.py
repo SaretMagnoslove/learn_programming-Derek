7e2c9914a1012cecf8cb41b1737d81966ada1515
SaretMagnoslove <magnoslove@gmail.com>
2017-12-06 20:38:59 +0200

    Init Commit

---
+++ Django/env_site1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\courses\youtube\learn to program - derak\django\env_site1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
